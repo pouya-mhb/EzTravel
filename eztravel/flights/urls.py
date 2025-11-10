285036c4d1d5a24de9176f2a476cafdfb0b4e781
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-11 00:16:46 +0330

    add book url

--- eztravel/flights/urls.py
+++ eztravel/flights/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import flight_search
+from .views import FlightBookingAPIView, flight_search
 
 urlpatterns = [
     path('search/', flight_search, name='flight-search'),
+    path("book/", FlightBookingAPIView.as_view(), name="flight-book"),
 ]
