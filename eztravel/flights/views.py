db66f08e746e884626e21f8d28552e31a065821e
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-11 00:16:58 +0330

    add booking views

--- eztravel/flights/views.py
+++ eztravel/flights/views.py
@@ -1,10 +1,11 @@
-from .models import Flight
-from .serializers import FlightSerializer
+from .models import Flight, FlightBooking
+from .serializers import FlightBookingSerializer, FlightSerializer
 from core.amadeus_client import search_flights
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from datetime import datetime
+from rest_framework.views import APIView
 
 @api_view(['GET'])
 def flight_search(request):
@@ -49,3 +50,35 @@ def flight_search(request):
         return Response(serializer.data)
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class FlightBookingAPIView(APIView):
+    def post(self, request):
+        flight_id = request.data.get("flight_id")
+        full_name = request.data.get("full_name")
+        email = request.data.get("email")
+        phone = request.data.get("phone")
+
+        if not flight_id or not all([full_name, email, phone]):
+            return Response({"error": "Missing required fields"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            flight = Flight.objects.get(id=flight_id)
+        except Flight.DoesNotExist:
+            return Response({"error": "Flight not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        booking = FlightBooking.objects.create(
+            flight=flight,
+            full_name=full_name,
+            email=email,
+            phone=phone
+        )
+
+        serializer = FlightBookingSerializer(booking)
+        return Response(
+            {
+                "message": "Booking successful (test mode)",
+                "reference_code": booking.reference_code,
+                "booking": serializer.data
+            },
+            status=status.HTTP_201_CREATED
+        )
